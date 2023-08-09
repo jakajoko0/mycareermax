962fc4b0a0f9931d63faa2a7fc2a47c9c12c330a
skalamera <skalamera@gmail.com>
2023-08-09 04:46:03 -0400

    added generate cover letter button

--- app.py
+++ app.py
@@ -62,6 +62,11 @@ def resume_analysis():
 
 
 # Route to handle the root URL (homepage)
+@app.route("/careerclick")
+def careerclick():
+    return render_template("careerclick.html")
+
+
 @app.route("/background-image")
 def background_image():
     return render_template("background_image.html")
@@ -187,26 +192,6 @@ def generate_cover_letter():
     return jsonify({"cover_letter": cover_letter})
 
 
-@app.route("/jobs_api.html")
-def index():
-    return open("jobs_api.html").read()
-
-
-@app.route("/search-rapidapi-jobs", methods=["POST"])
-def search_rapidapi_jobs():
-    location = request.form.get("location")
-    keywords = request.form.get("keywords")
-
-    query = f"{keywords} in {location}"
-    response = fetch_jobs_from_rapidapi(query)
-
-    print("RapidAPI Response:", response)  # Add this line for debugging
-
-    # Ensure the 'jobs' property always exists in the response
-    jobs = response.get("data", [])
-    return jsonify({"jobs": jobs})
-
-
 # INTERVIEW SIMULATOR
 # Question Generator Call to openai
 @app.route("/simulate-interview", methods=["POST"])
@@ -288,6 +273,86 @@ def analyze_answer():
 
     # Return the feedback as a JSON response
     return jsonify({"feedback": feedback})
+
+
+# CareerClick RapidAPI Routes
+# RapidAPI configuration
+RAPIDAPI_KEY = "04c645fbbdmshf581fe252de3b82p178cedjsn43d2da570f56"
+RAPIDAPI_HOST = "jsearch.p.rapidapi.com"
+RAPIDAPI_BASE_URL = "https://jsearch.p.rapidapi.com"
+headers = {"X-RapidAPI-Key": RAPIDAPI_KEY, "X-RapidAPI-Host": RAPIDAPI_HOST}
+
+
+@app.route("/fetch-job-listings", methods=["POST"])
+def fetch_job_listings():
+    query = request.json.get("query")
+    location = request.json.get("location")
+    # Construct the API URL
+    url = f"{RAPIDAPI_BASE_URL}/search?query={query} in {location}&page=1&num_pages=1"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Failed to fetch job listings"}), 500
+
+
+@app.route("/fetch-job-details", methods=["POST"])
+def fetch_job_details():
+    query = request.json.get("query")
+    # Construct the API URL for detailed information
+    url = f"{RAPIDAPI_BASE_URL}/search-filters?query={query}"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Failed to fetch job details"}), 500
+
+
+@app.route("/fetch-search-filters", methods=["POST"])
+def fetch_search_filters():
+    query = request.json.get("query")
+    # Construct the API URL for search filters
+    url = f"{RAPIDAPI_BASE_URL}/search-filters?query={query}"
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Failed to fetch search filters"}), 500
+
+
+# CareerClick OpenAI API Route
+@app.route("/career_click", methods=["POST"])
+def career_click():
+    # Extract the resume, job description, company name, and job title from the request
+    resume = request.json.get("resume")
+    job_description = request.json.get("job_description")
+    company_name = request.json.get("company_name")
+    job_title = request.json.get("job_title")
+
+    # Construct the conversation history as an array of messages
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant that generates personalized cover letters.",
+        },
+        {
+            "role": "user",
+            "content": f"Generate a cover letter for the following job. Resume: {resume}. Job description: {job_description}. Job title: {job_title}. Company name: {company_name},",
+        },
+    ]
+
+    # Make the API call to OpenAI's GPT-3.5 model
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        max_tokens=500,  # Set a limit on the length of the generated cover letter
+    )
+
+    # Extract the generated cover letter from the response
+    cover_letter = response.choices[0].message["content"].strip()
+
+    # Return the generated cover letter as a JSON response
+    return jsonify({"cover_letter": cover_letter})
 
 
 if __name__ == "__main__":
